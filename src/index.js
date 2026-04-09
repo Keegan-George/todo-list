cb91d94dcb1489f00780248ce83722095fe15780
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-04-09 15:30:05 -0400

    Removing commented code that is no longer needed

--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
 import "./styles.css";
 import {createTodoList} from "./scripts.js";
-
-
-// //Temporary for testing
-// const myToDoList = createTodoList("My To-Do List");
-
-// myToDoList.addTask("Pay Cell Phone Bill", "March 1", "March 5", 1, "");
-// console.log(myToDoList.getTasks());
-
-// myToDoList.addTask("Renew Driver's License", "March 2", "March 8", 2, "");
-// console.log(myToDoList.getTasks());
-
-// const task1ID = myToDoList.getTasks()[0].getID();
-// console.log(task1ID);
-
-// const task2ID = myToDoList.getTasks()[1].getID();
-// console.log(task2ID);
-
-
-// myToDoList.removeTask(task1ID);
-
-// console.log(myToDoList.getTasks());
-
